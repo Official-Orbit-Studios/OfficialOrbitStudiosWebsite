a3419cadb7a5adba73658c9aa8c565fcc3c69b64
Mariano Rodriguez <amr001.us@gmail.com>
2021-03-05 21:59:23 -0600

    First commit for everything, code being exported to GitHub

---
+++ README.md
@@ -0,0 +1,5 @@
+# The Official Orbit Studios Website
+
+All the code for our little corner of the web.
+
+Note: WIP
---
+++ static/scripts/script.js
@@ -0,0 +1,8 @@
+// Custom Dropdown Nav
+function openNav() {
+    document.getElementById("DropdownNav").style.height = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("DropdownNav").style.height = "0%";
+  }
